c7a1095dec9e02871c11b22bf478ebcd355062f2
MedbraT <MedbraT.PSY@yandex.ru>
2023-10-08 17:14:13 +0300

    Dev commit

--- app/src/main/java/com/practicum/sprint8_hw_res/MainActivity.kt
+++ app/src/main/java/com/practicum/sprint8_hw_res/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.practicum.sprint8_hw_res
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val buttClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Работает searchButton",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        searchButton.setOnClickListener(buttClickListener)
+
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        libraryButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Работает libraryButton",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Работает settingsButton",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
